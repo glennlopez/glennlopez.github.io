c59acb37d6dee359806333d657b31b04c8ae8ee0
Glenn <glennlopez@gmail.com>
2023-04-06 13:23:26 -0500

    fix ffor ios

--- js/store.js
+++ js/store.js
@@ -1,19 +1,18 @@
-let canadaAddress = document.getElementById("canada-address");
-let usaAddress = document.getElementById("usa-address");
+let canadaAddress = document.getElementById("canada-address"); // Get the DOM element for Canada address
+let usaAddress = document.getElementById("usa-address"); // Get the DOM element for USA address
 
-fetch('https://ipapi.co/json/')
-    .then(response => response.json())
-    .then(data => {
-        if (data.country === "CA") {
-            canadaAddress.style.display = "inline-block";
-            usaAddress.style.display = "none";
-        } else if (data.country === "US") {
-            canadaAddress.style.display = "none";
-            usaAddress.style.display = "inline-block";
-        }
-        else {
-            canadaAddress.style.display = "inline-block";
-            usaAddress.style.display = "inline-block";
+fetch('https://ipapi.co/json/') // Fetch the user's IP geolocation data
+    .then(response => response.json()) // Convert the response to a JSON object
+    .then(data => { // Handle the JSON object
+        if (data.country === "CA") { // If the user is in Canada
+            canadaAddress.style.display = "inline-block"; // Show the Canada address
+            usaAddress.style.display = "none"; // Hide the USA address
+        } else if (data.country === "US") { // If the user is in the USA
+            canadaAddress.style.display = "none"; // Hide the Canada address
+            usaAddress.style.display = "inline-block"; // Show the USA address
+        } else { // If the user is in any other country
+            canadaAddress.style.display = "inline-block"; // Show the Canada address
+            usaAddress.style.display = "inline-block"; // Show the USA address
         }
     })
-    .catch(error => console.error(error));
+    .catch(error => console.error(error)); // Log any errors to the console
