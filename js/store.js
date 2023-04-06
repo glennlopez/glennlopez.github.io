ecdbb2878e7f6d0d43e092af1cb9eff9821cc62c
Glenn <glennlopez@gmail.com>
2023-04-06 12:14:45 -0500

    changed to display both if location unknown

--- js/store.js
+++ js/store.js
@@ -6,7 +6,13 @@ fetch('https://ipapi.co/json/')
     .then(data => {
         if (data.country === "CA") {
             canadaAddress.style.display = "inline-block";
+            usaAddress.style.display = "none";
         } else if (data.country === "US") {
+            canadaAddress.style.display = "none";
+            usaAddress.style.display = "inline-block";
+        }
+        else {
+            canadaAddress.style.display = "inline-block";
             usaAddress.style.display = "inline-block";
         }
     })
