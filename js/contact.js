dd6342a982047f760730221dcf55d1bc34f6cf22
Glenn Lopez <glennlopez@gmail.com>
2023-12-18 12:35:42 -0600

    fixed spacing and added Show/Hide short url

--- js/contact.js
+++ js/contact.js
@@ -44,12 +44,28 @@ function checkButtonState() {
 checkButtonState();
 
 // JavaScript to handle the toggle functionality
-document.getElementById('toggleList').addEventListener('click', function (e) {
-    e.preventDefault();
-    var listWrapper = document.querySelector('.short-url-list-wrapper');
-    if (listWrapper.style.display === 'none' || listWrapper.style.display === '') {
-        listWrapper.style.display = 'block';
-    } else {
-        listWrapper.style.display = 'none';
-    }
+// document.getElementById('toggleList').addEventListener('click', function (e) {
+//     e.preventDefault();
+//     var listWrapper = document.querySelector('.short-url-list-wrapper');
+//     if (listWrapper.style.display === 'none' || listWrapper.style.display === '') {
+//         listWrapper.style.display = 'block';
+//     } else {
+//         listWrapper.style.display = 'none';
+//     }
+// });
+
+// Toggle the visibility of Short-URL list
+document.addEventListener("DOMContentLoaded", function () {
+    var toggleListLink = document.getElementById("toggleListLink");
+    var toggleListContainer = document.getElementById("toggleListContainer");
+
+    toggleListLink.addEventListener("click", function () {
+        if (toggleListContainer.style.display === "none" || toggleListContainer.style.display === "") {
+            toggleListContainer.style.display = "block";
+            toggleListLink.textContent = "ⓘ Hide all short URLs";
+        } else {
+            toggleListContainer.style.display = "none";
+            toggleListLink.textContent = "ⓘ Show all short URLs";
+        }
+    });
 });
